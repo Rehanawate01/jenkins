d1d8cbc22887a2b4ad3e1c2498a9ddbffd3181fe
--add <rehanawate3@gmail.com>
2020-12-10 16:31:16 +0530

    initial2

--- Hello.java
+++ Hello.java
@@ -2,11 +2,10 @@ public class Hello {
     
 
     public static void main(String args[]) {
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+     
+     for(int i=0;i<3;i++)
+        System.out.println(i);
+    }
+      
     }
 }
